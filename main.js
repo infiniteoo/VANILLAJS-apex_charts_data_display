ae32c2cb5367e4b2a0fd4009e2b8abc02974d241
Troy Dorman <troydorman@gmail.com>
2021-08-03 06:45:07 -0600

    defined our chart options - height/width type and colors

--- main.js
+++ main.js
@@ -1,5 +1,14 @@
 // chart options
-const options = {};
+const options = {
+    chart: {
+        height: 450,
+        width: '100%',
+        type: 'bar',
+        background: '#f4f4f4',
+        foreColor: '#333'
+    },
+    
+};
 
 // init chart
 const chart = new ApexCharts(document.querySelector("#chart"), options);
