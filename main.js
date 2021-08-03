a4cc04352510c4a7f0a001f92cd2b09334194519
Troy Dorman <troydorman@gmail.com>
2021-08-03 06:46:29 -0600

    defined the name and data points for our chart

--- main.js
+++ main.js
@@ -1,13 +1,21 @@
 // chart options
 const options = {
-    chart: {
-        height: 450,
-        width: '100%',
-        type: 'bar',
-        background: '#f4f4f4',
-        foreColor: '#333'
+  chart: {
+    height: 450,
+    width: "100%",
+    type: "bar",
+    background: "#f4f4f4",
+    foreColor: "#333",
+  },
+  series: [
+    {
+      name: "Population",
+      data: [
+        8550405, 3971883, 2720546, 2296224, 1567442, 1563025, 1469845, 1394928,
+        1300092, 1026908,
+      ],
     },
-    
+  ],
 };
 
 // init chart
